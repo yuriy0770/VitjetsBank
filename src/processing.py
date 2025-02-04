608d4e125b28fde8d80ab32e50e42d8d770b3ab4
origin <yura28049119@yandex.ru>
2025-02-05 02:32:18 +0300

    Добавление коммитов

--- src/processing.py
+++ src/processing.py
@@ -2,11 +2,14 @@ from typing import List
 
 
 def filter_by_state(dict_lists: List, state='EXECUTED') -> List:
+    '''Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+state
+ соответствует указанному значению.'''
     for i in dict_lists:
         i["state"] = state
     return dict_lists
 
 
 def sort_by_date(dict_lists: List, date=True) -> List:
+    '''Функция должна возвращать новый список, отсортированный по дате'''
     return sorted(dict_lists, key=lambda x: x["date"], reverse=date)
-
