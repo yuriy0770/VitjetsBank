e349d7fbcb8945b4a6ec31462f5bb177b489b71e
origin <yura28049119@yandex.ru>
2025-02-05 05:25:24 +0300

    Исправил и доработал

--- src/processing.py
+++ src/processing.py
@@ -2,14 +2,15 @@ from typing import List
 
 
 def filter_by_state(dict_lists: List, state='EXECUTED') -> List:
-    '''Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
-
-    сответствует указанному значению.'''
-    for i in dict_lists:
-        i["state"] = state
-    return dict_lists
+    '''Функция возвращает новый список словарей,
+     содержащий только те словари, у которых ключ
+     state соответствует указанному значению.'''
+    return [i for i in dict_lists if state in list(i.values())]
 
 
-def sort_by_date(dict_lists: List, date=True) -> List:
-    '''Функция должна возвращать новый список, отсортированный по дате'''
-    return sorted(dict_lists, key=lambda x: x["date"], reverse=date)
+def sort_by_date(dict_lists1: List, date=True) -> List:
+    '''Функция возвращает новый список, отсортированный по date'''
+    return sorted(dict_lists1, key=lambda x: x["date"], reverse=date)
+
+
+
