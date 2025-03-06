7bd01c82f4c5059b8cf9bc1f2e8c8670659cef46
origin <yura28049119@yandex.ru>
2025-03-06 03:29:43 +0300

    homework12.1

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-import time
 
-
-def wrapper(f):
-    def inner(*args):
-        return f(*args)*3
-    return inner
-
-@wrapper
-def q(w, e):
-    return w + e
-
-print(q(1, 2))
+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
